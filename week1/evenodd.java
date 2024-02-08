c66ce2e4698e35c3d432020f6e09f42450efc4b5
SriVyshnavi05 <158752369+SriVyshnavi05@users.noreply.github.com>
2024-02-08 10:25:16 +0530

    Update evenodd.java

--- week1/evenodd.java
+++ week1/evenodd.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
 class Evenorodd
 {
-  static void find(int a)
-  {
-    if(a%2==0)
-    {
-System.out.print("The value "+a+" is a even number");
-    }
-    else
-    {
-      System.out.print("The value "+a+" is a odd number");
-    }
-  }
-  public static void main(String[] args)
-  {
-    int a;
-    System.out.print("Enter the value of a : ");
-    Scanner input=new Scanner(System.in);
-    a=input.nextInt();
-    input.close();
-    find(a);
-  }
+void find(int a)
+{
+if(a==0)
+{
+System.out.print(a+" is neither even nor odd\n");
 }
+else if(a%2==0)
+{
+System.out.print(a+" is a even number\n");
+}
+else
+{
+System.out.print(a+" is a odd number\n");
+}
+}
+public static void main(String[] args)
+{
+int a;
+System.out.print("Enter the value of a : ");
+Scanner input=new Scanner(System.in);
+a=input.nextInt();
+input.close();
+Evenorodd get=new Evenorodd();
+get.find(a);
+}
+}
+
