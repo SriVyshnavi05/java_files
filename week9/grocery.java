11f6837c2b1852234a92eb2a06e7d536dcb7b2dd
SriVyshnavi05 <158752369+SriVyshnavi05@users.noreply.github.com>
2024-04-25 09:13:04 +0530

    Create grocery.java

---
+++ week9/grocery.java
@@ -0,0 +1,19 @@
+class GroceryException extends Exception{
+    GroceryException(){
+        System.out.println("The quality of the groceries is not up to the mark\nVery disappointed.");
+    }
+}
+public class Main {
+    public static void main(String[] args) throws GroceryException{
+      int groceryQuality=35;
+        if(groceryQuality>80){
+            System.out.println("The quality of the groceries are good.");
+        }
+        else{
+            try{
+                throw new GroceryException();
+            }catch(GroceryException e){
+            }
+        }
+   }
+}
