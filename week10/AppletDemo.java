bf82c1c8f8f4775e61e5241d92b390bc4435f792
SriVyshnavi05 <158752369+SriVyshnavi05@users.noreply.github.com>
2024-04-30 17:45:31 +0530

    Update AppletDemo.java

--- week10/AppletDemo.java
+++ week10/AppletDemo.java
@@ -4,7 +4,7 @@ import java.applet.*;
 /*
 <applet code="Demo.class" width="1000" height="1000"> </applet>
 */
-public class ApppletDemo extends Applet {
+public class Demo extends Applet {
 	
 	public void paint(Graphics g) {
 		g.drawLine(10,10,300,300);
